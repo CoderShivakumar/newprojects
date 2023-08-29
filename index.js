e63112a8d6155c753aa2b0b6fec0c4404b9c3b70
CoderShivakumar <134090630+CoderShivakumar@users.noreply.github.com>
2023-08-29 20:38:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var content=["Good Friend","Bad Friend"];
+var btn=document.querySelector("btn");
+var up=document.getElementById("update");
+btn.addEventListener("click",()=>
+{
+    document.getElementById("update").innerHTML="hello";
+});
